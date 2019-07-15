fd450f9e680d9886b0a47d07f49493720b298607
Norbert Cyran <cyran.norbert97@gmail.com>
2019-07-15 02:11:43 +0200

    Add API docs

--- weird_text/urls.py
+++ weird_text/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('v1/', include('core.urls'))
+    path('v1/', include('core.urls')),
+    path('docs/', include_docs_urls(title='Weird encoding API'))
 ]
